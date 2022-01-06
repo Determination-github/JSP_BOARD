7851017deab8f2520c6e2bd978af7918173745ef
Determination-github <ungseopkim@naver.com>
2022-01-06 23:47:16 +0900

    docs : Change the way of connection
    기존 CommandAction을 통해 view를 연결하던 코드를 Action과 ActionForward 클래스로 대체
    (아직 오류로 인해 동작하지 않음)

--- JSP_BOARD/src/jsp/front/controller/MemberController.java
+++ JSP_BOARD/src/jsp/front/controller/MemberController.java
@@ -14,9 +14,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jsp.common.action.Action;
+import jsp.common.action.ActionForward;
+import jsp.member.action.MemberFormChangeAction;
+
 public class MemberController extends HttpServlet {
 	
-	private Map map = new HashMap<>();
+	private HashMap<String, Action> commandMap;
 	
 	//명령어와 처리클래스가 매핑된 properties 파일을 읽어 Map객체에 저장
 	public void init(ServletConfig config) throws ServletException { //서블렛 초기화
@@ -24,6 +28,7 @@ public class MemberController extends HttpServlet {
 		String props = config.getInitParameter("propertyConfig");
 		Properties properties = new Properties(); //명령어와 처리클래스의 매핑 정보를 저장할 프로퍼티 객체
 		FileInputStream fis = null;
+		commandMap = new HashMap<String, Action>();
 		try {
 			fis = new FileInputStream(props); //command.properties 파일을 fis에 넣기
 			properties.load(fis); //command.properties를 properties 객체에 저장하기
@@ -43,12 +48,19 @@ public class MemberController extends HttpServlet {
 		while(iterator.hasNext()) {
 			String key = (String)iterator.next(); //프로퍼티 key 값
 			String value = properties.getProperty(key); //프로터피 value 값
-			System.out.println(key);
-			System.out.println(value);
+			System.out.println(key); ///LoginForm.do
+			System.out.println(value); //jsp.member.action.MemberFormChangeAction
 			try {
-				Class commandClass = Class.forName(value); //해당 문자열을 클래스로 만들기
-				Object commandInstance = commandClass.getDeclaredConstructor().newInstance(); //문자열로 만든 클래스를 객체화
-				map.put(key, commandInstance); //map에 키값과 value값 객체 저장
+				Class commandClass = Class.forName(value); //해당 문자열을 클래스로 만들기, jsp.member.action.MemberFormChangeAction
+				Action actionInstance = (Action)commandClass.getDeclaredConstructor().newInstance();
+				System.out.println(actionInstance);
+				
+				if(value.equals("jsp.member.action.MemberFormChangeAction")) {
+					MemberFormChangeAction changeAction = (MemberFormChangeAction) actionInstance;
+					changeAction.setCommand(key);
+				}
+				
+				commandMap.put(key, actionInstance); //map에 키값과 value값 객체 저장
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -58,31 +70,35 @@ public class MemberController extends HttpServlet {
 	//post나 get요청이 들어오면 실행되는 메소드
 	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String URI = null;
-		String view = null;
-		CommandAction com = null;
+		ActionForward forward = null;
+		Action action = null;
+		
 		try {
-			URI = request.getRequestURI();
-			System.out.println(URI);
-			String command = URI.substring(URI.lastIndexOf("/"));
-			System.out.println(command);
-			com = (CommandAction)map.get(command); //command = KEY값
-			System.out.println(com);
-			if(com == null) {
+			URI = request.getRequestURI(); 
+			System.out.println(URI);// /JSP_BOARD/LoginForm.do
+			String command = URI.substring(URI.lastIndexOf("/")+1);
+			System.out.println(command);// /LoginForm.do
+			action = commandMap.get(command); //command = KEY값
+			System.out.println(action);
+			if(action == null) {
 				System.out.println("not found : "+command);
 				return;
 			}
-			view = com.requestPro(request, response);
-			System.out.println(view);
-		} catch (Throwable e) {
-			throw new ServletException(e);
+			
+			forward = action.execute(request, response);
+			System.out.println(forward);
+			
+			if(forward != null) {
+				if(forward.isRedirect()) {
+					response.sendRedirect(forward.getPath());
+				} else {
+					RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+					dispatcher.forward(request, response);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		if(view == null) {
-			return;
-		}
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
-		dispatcher.forward(request, response);
 	}	
 	
 	@Override

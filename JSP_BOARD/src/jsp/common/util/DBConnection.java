3acd96a4d0cc8363619369d44a809319c708874c
Determination-github <ungseopkim@naver.com>
2022-01-06 23:42:50 +0900

    docs : Change oracle ID/PW, Rename con to conn
    오라클 계정변경, Connection 객체 이름 변경

--- JSP_BOARD/src/jsp/common/util/DBConnection.java
+++ JSP_BOARD/src/jsp/common/util/DBConnection.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 
 public class DBConnection {
 	//물리적으로 떨어져 있는 오라클 서버에 연결통로를 확보
-	Connection con = null;
+	Connection conn = null;
 	//자바코드에서 작성한 select문을 전달함
 	Statement  stmt = null;
 	//프로시저를 전담하는 인터페이스 선언
@@ -17,11 +17,11 @@ public class DBConnection {
 	//오라클에게 전달된 select문을 처리한 결과를 꺼내기 위해서 커서를 조작해야 하는데
 	//그 때 커서를 이동하는데 필요한 메소드를 선언한 인터페이스
 	ResultSet  rs   = null;
-	public static final String _DRIVER = "oracle.jdbc.driver.OracleDriver";//ojdbc6.jar  의존관계, 의존성 주입 제조사
+	public static final String _DRIVER = "oracle.jdbc.OracleDriver";//ojdbc8.jar  의존관계, 의존성 주입 제조사
 	//서버측 아이피, 서버측 포트번호, 오라클 SID
-	public static final String _URL    = "jdbc:oracle:thin:@127.0.0.1:1522:orcl";
-	public static final String _USER   = "scott";
-	public static final String _PW     = "tiger";
+	public static final String _URL    = "jdbc:oracle:thin:@127.0.0.1:1522:ORCL";
+	public static final String _USER   = "jsp_board";
+	public static final String _PW     = "1234";
 	private static DBConnection dbMgr = null;
 	
 	//싱글톤 패턴
@@ -36,33 +36,33 @@ public class DBConnection {
 	public Connection getConnection() {
 		try {
 			Class.forName(_DRIVER);
-			con = DriverManager.getConnection(_URL, _USER, _PW);
+			conn = DriverManager.getConnection(_URL, _USER, _PW);
 		} catch (Exception e) {
 			System.out.println("드라이버 클래스를 찾을 수 없습니다.");
 			System.out.println(e.toString());
 		}
-		return con;
+		return conn;
 	}
 /*
  * 사용한 자원 반납하기 
  * 생성된 역순으로 반납
  */
 	
-	public void freeConnection(Connection con, PreparedStatement pstmt, ResultSet rs) {//select일때
+	public void freeConnection(Connection conn, PreparedStatement pstmt, ResultSet rs) {//select일때
 		try {
 			if(rs !=null) rs.close();
 			if(pstmt !=null) pstmt.close();
-			if(con !=null) con.close();
+			if(conn !=null) conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	//PreparedStatement 동적쿼리에서 사용(권장사항) SELECT * FROM member WHERE id=?
-	public void freeConnection(Connection con, PreparedStatement pstmt) {//INSERT|UPDATE|DELETE
+	public void freeConnection(Connection conn, PreparedStatement pstmt) {//INSERT|UPDATE|DELETE
 		try {
 			if(pstmt !=null) pstmt.close();
-			if(con !=null) con.close();
+			if(conn !=null) conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -70,10 +70,10 @@ public class DBConnection {
 	
 	
 	//CallableStatement 프로시저에 사용
-	public void freeConnection(Connection con, CallableStatement cstmt) {//INSERT|UPDATE|DELETE
+	public void freeConnection(Connection conn, CallableStatement cstmt) {//INSERT|UPDATE|DELETE
 		try {
 			if(cstmt !=null) cstmt.close();
-			if(con !=null) con.close();
+			if(conn !=null) conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

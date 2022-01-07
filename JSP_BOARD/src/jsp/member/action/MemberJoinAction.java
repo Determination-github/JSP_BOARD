ab52b15f7cd6f0e2e30536e9bb29f75961b0dfc3
Determination-github <ungseopkim@naver.com>
2022-01-07 21:35:00 +0900

    feature : ADD MemberJoinAction.java
    회원가입 및 결과처리를 위한 클래스 추가

---
+++ JSP_BOARD/src/jsp/member/action/MemberJoinAction.java
@@ -0,0 +1,38 @@
+package jsp.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jsp.common.action.Action;
+import jsp.common.action.ActionForward;
+import jsp.member.model.members.MemberBean;
+import jsp.member.model.members.MemberDAO;
+
+public class MemberJoinAction implements Action {
+	
+	
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		ActionForward forward = new ActionForward();
+		
+		MemberDAO mDAO = MemberDAO.getInstance();
+		
+		//입력된 정보 자바빈으로 세팅하기
+		MemberBean member = new MemberBean();
+		member.setMemberID(request.getParameter("memberID"));
+		member.setMemberPWD(request.getParameter("memberPWD"));
+		member.setMemberName(request.getParameter("memberName"));
+		member.setMemberEmail(request.getParameter("memberEmail"));
+		
+		//insert 실행
+		mDAO.joinMember(member); 
+		
+		//회원가입 후 화면 전환
+		forward.setRedirect(true);
+		forward.setPath("ResultForm.do");
+		
+		return forward;
+	}
+
+}

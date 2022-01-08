87022042a92a316fd9b7d5400802bb14c8d3a203
Determination-github <ungseopkim@naver.com>
2022-01-08 17:18:24 +0900

    feature : ADD Logout class
    로그아웃 기능을 처리할 자바 클래스 구현

---
+++ JSP_BOARD/src/jsp/member/action/MemberLogoutAction.java
@@ -0,0 +1,26 @@
+package jsp.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jsp.common.action.Action;
+import jsp.common.action.ActionForward;
+import jsp.member.model.members.MemberDAO;
+
+public class MemberLogoutAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward = new ActionForward();
+		
+		//세션에 담긴 아이디값 삭제
+		request.getSession().removeAttribute("memberID");
+		
+		//메인화면으로 이동
+		forward.setRedirect(true);
+		forward.setPath("main.do");
+		
+		return forward;
+	}
+}
